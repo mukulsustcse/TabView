cf5e0d3f224a3fad4116e327ed31e4a2f34c9d03
M. A. Alim Mukul <mukul.cse2010@gmail.com>
2022-08-29 13:57:38 +0200

    add TabItems

--- TabView/ContentView.swift
+++ TabView/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var tabIndex = 1
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        
+        
+        TabView(selection: $tabIndex) {
+            Text("This is tab 1")
+                .tabItem {
+                    
+                    VStack {
+                        Image(systemName: "pencil")
+                        Text("Tab 1")
+                    }
+                }
+                .tag(1)
+            
+            VStack {
+                Text("Hello")
+                Text("This is tab 2")
+                
+            }
+            .tabItem {
+                Image(systemName: "star")
+                Text("Tab 2")
+            }
+            .tag(2)
+        }
     }
 }
 
