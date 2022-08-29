2834485030c80862511d552ece0597deabd23363
M. A. Alim Mukul <mukul.cse2010@gmail.com>
2022-08-29 13:40:23 +0200

    Initial Commit

---
+++ TabView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TabView
+//
+//  Created by M. A. Alim Mukul on 29.08.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TabView/TabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewApp.swift
+//  TabView
+//
+//  Created by M. A. Alim Mukul on 29.08.22.
+//
+
+import SwiftUI
+
+@main
+struct TabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
